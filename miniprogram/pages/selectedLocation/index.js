c6bd8ae86aaa6d4c1cb39cba307076df61451d72
vicksiyi <1724264854@qq.com>
2019-07-12 17:36:42 +0800

    Seat data connects to the database

--- miniprogram/pages/selectedLocation/index.js
+++ miniprogram/pages/selectedLocation/index.js
@@ -1,4 +1,6 @@
-const { $Message } = require('../../dist/base/index');
+const {
+  $Message
+} = require('../../dist/base/index');
 Page({
 
   /**
@@ -11,286 +13,43 @@ Page({
     onS: '',
     itemS: '',
     selectSeat: '',
-    seatAll: [{
-        A: [{
-            name: '黄艳锋',
-            nameSeat: 'H1'
-          },
-          {
-            name: '范金梅',
-            nameSeat: 'H1'
-          },
-          {
-            name: '王育涵',
-            nameSeat: 'H3'
-          },
-          {
-            name: '黄丽映',
-            nameSeat: 'H4'
-          },
-          {
-            name: '谭焕星',
-            nameSeat: 'H5'
-          },
-          {
-            name: '张坤涛',
-            nameSeat: 'H6'
-          },
-          {
-            name: '肖文',
-            nameSeat: 'H7'
-          },
-          {
-            name: '毛瑜',
-            nameSeat: 'H8'
-          }
-        ],
-        B: [{
-            name: '张伟均',
-            nameSeat: 'G1'
-          },
-          {
-            name: '李镘晴',
-            nameSeat: 'G2'
-          },
-          {
-            name: '彭楚瑶',
-            nameSeat: 'G3'
-          },
-          {
-            name: '林楚羽',
-            nameSeat: 'G4'
-          },
-          {
-            name: '温春慧',
-            nameSeat: 'G5'
-          },
-          {
-            name: '谭颖莎',
-            nameSeat: 'G6'
-          },
-          {
-            name: '林诗慧',
-            nameSeat: 'G7'
-          },
-          {
-            name: '张丽萍',
-            nameSeat: 'G8'
-          }
-        ]
-      },
-      {
-        A: [{
-            name: '陈安娜',
-            nameSeat: 'F1'
-          },
-          {
-            name: '李麦琳',
-            nameSeat: 'F2'
-          },
-          {
-            name: '钟漫茵',
-            nameSeat: 'F3'
-          },
-          {
-            name: '李满玉',
-            nameSeat: 'F4'
-          },
-          {
-            name: '陈灿英',
-            nameSeat: 'F5'
-          },
-          {
-            name: '高铭',
-            nameSeat: 'F6'
-          },
-          {
-            name: '赵康展',
-            nameSeat: 'F7'
-          },
-          {
-            name: '甘捷琳',
-            nameSeat: 'F8'
-          }
-        ],
-        B: [{
-            name: '孔文熙',
-            nameSeat: 'E1'
-          },
-          {
-            name: '李晓纯',
-            nameSeat: 'E2'
-          },
-          {
-            name: '温绣琼',
-            nameSeat: 'E3'
-          },
-          {
-            name: '潘春霞',
-            nameSeat: 'E4'
-          },
-          {
-            name: '钟文均',
-            nameSeat: 'E5'
-          },
-          {
-            name: '林雅冰',
-            nameSeat: 'E6'
-          },
-          {
-            name: '陈如娇',
-            nameSeat: 'E7'
-          },
-          {
-            name: '张少敏',
-            nameSeat: 'E8'
-          }
-        ]
-      },
-      {
-        A: [{
-            name: '杨惠琼',
-            nameSeat: 'D1'
-          },
-          {
-            name: '赖晓珊',
-            nameSeat: 'D2'
-          },
-          {
-            name: '陈名森',
-            nameSeat: 'D3'
-          },
-          {
-            name: '陈艳萍',
-            nameSeat: 'D4'
-          },
-          {
-            name: '黄阳燕',
-            nameSeat: 'D5'
-          },
-          {
-            name: '莫伟宁',
-            nameSeat: 'D6'
-          },
-          {
-            name: '张丽雪',
-            nameSeat: 'D7'
-          },
-          {
-            name: '冯越',
-            nameSeat: 'D8'
-          }
-        ],
-        B: [{
-            name: '敖雅宝',
-            nameSeat: 'C1'
-          },
-          {
-            name: '吴妍旋',
-            nameSeat: 'C2'
-          },
-          {
-            name: '汪晓彤',
-            nameSeat: 'C3'
-          },
-          {
-            name: '李逢吉',
-            nameSeat: 'C4'
-          },
-          {
-            name: '张庆鸿',
-            nameSeat: 'C5'
-          },
-          {
-            name: '刘新豪',
-            nameSeat: 'C6'
-          },
-          {
-            name: '谢鸿鑫',
-            nameSeat: 'C7'
-          },
-          {
-            name: '宋智广',
-            nameSeat: 'C8'
-          }
-        ]
-      },
-      {
-        A: [{
-            name: '梁永励',
-            nameSeat: 'B1'
-          },
-          {
-            name: '刘妍',
-            nameSeat: 'B2'
-          },
-          {
-            name: '王嘉欣',
-            nameSeat: 'B3'
-          },
-          {
-            name: '刘爱丽',
-            nameSeat: 'B4'
-          },
-          {
-            name: '周月晓',
-            nameSeat: 'B5'
-          },
-          {
-            name: '林彭轩',
-            nameSeat: 'B6'
-          },
-          {
-            name: '周俊余',
-            nameSeat: 'B7'
-          },
-          {
-            name: '刘淸茹',
-            nameSeat: 'B8'
-          }
-        ],
-        B: [{
-            name: '',
-            nameSeat: 'A1'
-          },
-          {
-            name: '范泽润',
-            nameSeat: 'A2'
-          },
-          {
-            name: '陈卓斌',
-            nameSeat: 'A3'
-          },
-          {
-            name: '官春发',
-            nameSeat: 'A4'
-          },
-          {
-            name: '于宝',
-            nameSeat: 'A5'
-          },
-          {
-            name: '李桂斌',
-            nameSeat: 'A6'
-          },
-          {
-            name: '*',
-            nameSeat: 'A7'
-          },
-          {
-            name: '*',
-            nameSeat: 'A8'
-          }
-        ]
-      }
-    ]
+    seatAll: [],
+    seatClumn: [],
+    spinShow: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-
+    let _this = this
+    const db = wx.cloud.database()
+    let tempSeatClumn = []
+    let tempSeatAll = []
+    _this.setData({
+      spinShow: true
+    })
+    db.collection('sXuns_seatClumn').get().then(res => {
+      for (let i = 0; i < res.data.length; i++) {
+        tempSeatClumn.push(res.data[i])
+      }
+      _this.setData({
+        seatClumn: tempSeatClumn
+      })
+    })
+    db.collection('sXuns_seatAll').get().then(res => {
+      for (let i = 0; i < res.data.length; i++) {
+        tempSeatAll.push(res.data[i])
+      }
+      _this.setData({
+        seatAll: tempSeatAll
+      })
+    })
+    setTimeout(res => {
+      _this.setData({
+        spinShow: false
+      })
+    }, 1500)
   },
   clickSeat: function(res) {
     let _this = this
@@ -313,12 +72,9 @@ Page({
     } else {
       if (_this.data.selectSeat != temp.nameSeat) {
         _this.setData({
-          allS: all,
-          onS: on,
-          itemS: item,
           selectSeat: temp.nameSeat
         })
-      }else{
+      } else {
         _this.setData({
           allS: all,
           onS: on,
@@ -327,13 +83,14 @@ Page({
         })
       }
     }
+    console.log("数据", _this.data.nameSeatOn)
   },
   handleClose1() {
     this.setData({
       visible1: false
     });
   },
-  submitApplication:function(){
+  submitApplication: function() {
     $Message({
       content: '已提交后台审核',
       type: 'success'
